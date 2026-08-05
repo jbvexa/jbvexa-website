1b61d186b18bb5193afd0d4a82b4d93fe66ab86c
Binod Das <binod@jbvexa.com>
2026-08-05 17:17:13 +0530

    Add Google Analytics integration

--- components/GoogleAnalytics.tsx
+++ components/GoogleAnalytics.tsx
@@ -2,30 +2,39 @@
 
 import Script from "next/script";
 
-const GA_MEASUREMENT_ID =
-  process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID;
-
 export default function GoogleAnalytics() {
-  if (!GA_MEASUREMENT_ID) return null;
+  console.log(
+    "GA ID =",
+    process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID
+  );
 
   return (
     <>
+      <div
+        style={{
+          position: "fixed",
+          bottom: 20,
+          right: 20,
+          background: "red",
+          color: "white",
+          padding: 10,
+          zIndex: 99999,
+        }}
+      >
+        {process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID || "NO GA ID"}
+      </div>
+
       <Script
-        src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}`}
         strategy="afterInteractive"
       />
 
-      <Script id="google-analytics" strategy="afterInteractive">
+      <Script id="ga" strategy="afterInteractive">
         {`
           window.dataLayer = window.dataLayer || [];
-
           function gtag(){dataLayer.push(arguments);}
-
           gtag('js', new Date());
-
-          gtag('config', '${GA_MEASUREMENT_ID}', {
-            page_path: window.location.pathname,
-          });
+          gtag('config', '${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}');
         `}
       </Script>
     </>
